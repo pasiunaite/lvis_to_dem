3321a09bb1f07e0271a4a3f529959dcfd5a46ee1
pasiunaite <pasgabyte@gmail.com>
2020-03-01 19:57:15 +0000

    gap filling and smoothing

--- scripts/full_dem.py
+++ scripts/full_dem.py
@@ -18,9 +18,10 @@ from os import getenv
 from lvis_ground import lvisGround
 from dem import DEM
 import numpy as np
+from scipy.signal import fftconvolve
 import gc
-from copy import deepcopy
 from lxml import etree
+from osgeo import gdal
 
 
 
@@ -38,6 +39,54 @@ def getCmdArgs():
     args = parser.parse_args()
     return args
 
+def merge_tiles2(year):
+    # Change directory into the outputs file dir
+    os.chdir(r"../outputs/" + str(year))
+    print("Merging files from " + str(year))
+
+    # Build a GDAL virtual image from all the rasters
+    build_vrt_cmd = "gdalbuildvrt -srcnodata -999.0 -overwrite -r cubic " + str(year) + "_avg.vrt ./*_dem.tif"
+    os.system(build_vrt_cmd)
+
+    # Add a pixel function to the VRT to average overlapping pixels.
+    # Modified from: https://gis.stackexchange.com/questions/350233/how-can-i-modify-a-vrtrasterband-sub-class-etc-from-python
+    vrt_tree = etree.parse(str(year) + "_avg.vrt")
+    vrt_root = vrt_tree.getroot()
+    vrtband1 = vrt_root.findall(".//VRTRasterBand[@band='1']")[0]
+
+    vrtband1.set("subClass", "VRTDerivedRasterBand")
+    pixelFunctionType = etree.Element('PixelFunctionType')
+    pixelFunctionType.text = "average"
+    vrtband1.insert(0, pixelFunctionType)
+    pixelFunctionLanguage = etree.Element('PixelFunctionLanguage')
+    pixelFunctionLanguage.text = "Python"
+    vrtband1.insert(1, pixelFunctionLanguage)
+    pixelFunctionCode = etree.Element('PixelFunctionCode')
+    pixelFunctionCode.text = etree.CDATA("""
+import numpy as np
+def average(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,raster_ysize, buf_radius, gt, **kwargs):
+    x = np.ma.masked_equal(in_ar, -999)
+    np.mean(x, axis = 0, out = out_ar, dtype = 'Float32')
+    mask = np.all(x.mask, axis = 0)
+    out_ar[mask]=-999
+    """)
+    vrtband1.insert(2, pixelFunctionCode)
+    vrt_tree.write(str(year) + "_avg.vrt")
+
+    # Merge all the rasters, where pixels overlap - average them
+    merge_cmd = "gdal_translate --config GDAL_VRT_ENABLE_PYTHON YES -a_nodata -999.0 " + \
+                str(year) + "_avg.vrt " + str(year) + "_merged.tif"
+    os.system(merge_cmd)
+
+    # ---------- GAP FILLING ---------
+    # Fill gaps using
+    gap_fill_cmd = "gdal_fillnodata.py -md 100000 -nomask -si 5 " + str(year) + "_merged.tif " + str(year) + "_filled.tif"
+    os.system(gap_fill_cmd)
+
+    # Go back to the main working directory
+    #os.chdir(r"../../scripts")
+    return
+
 
 def merge_tiles(year):
     # Change directory into the outputs file dir
@@ -45,8 +94,8 @@ def merge_tiles(year):
     print("Merging files from " + str(year))
 
     # Build a GDAL virtual image from all the rasters
-    build_vrt_cmd = "gdalbuildvrt -srcnodata -999.0 -overwrite " + str(year) + "_avg.vrt ./*_dem.tif"
-    os.system(build_vrt_cmd)
+    build_vrt_cmd = "gdalbuildvrt -srcnodata -999.0 -r cubic " + str(year) + "_avg.vrt ./*_dem.tif"
+    #os.system(build_vrt_cmd)
 
     # Add a pixel function to the VRT to average overlapping pixels.
     # Modified from: https://gis.stackexchange.com/questions/350233/how-can-i-modify-a-vrtrasterband-sub-class-etc-from-python
@@ -63,26 +112,83 @@ def merge_tiles(year):
     vrtband1.insert(1, pixelFunctionLanguage)
     pixelFunctionCode = etree.Element('PixelFunctionCode')
     pixelFunctionCode.text = etree.CDATA("""
-    import numpy as np
+import numpy as np
 
-    def average(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,raster_ysize, buf_radius, gt, **kwargs):
-        x = np.ma.masked_equal(in_ar, -999)
-        np.mean(x, axis = 0, out = out_ar, dtype = 'Float32')
-        mask = np.all(x.mask, axis = 0)
-        out_ar[mask]=-999
+def average(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize, buf_radius, gt, **kwargs):
+    x = np.ma.masked_equal(in_ar, -999)
+    np.mean(x, axis = 0, out = out_ar, dtype = 'Float32')
+    mask = np.all(x.mask, axis = 0)
+    out_ar[mask]=-999
     """)
     vrtband1.insert(2, pixelFunctionCode)
-    vrt_tree.write(str(year) + "_avg.vrt")
+    #vrt_tree.write(str(year) + "_avg.vrt")
 
     # Merge all the rasters, where pixels overlap - average them
-    merge_cmd = "gdal_translate --config GDAL_VRT_ENABLE_PYTHON YES -a_nodata -999.0 " + \
-                str(year) + "_avg.vrt " + str(year) + "_merged.tif"
+    merge_cmd = "gdal_translate --config GDAL_VRT_ENABLE_PYTHON YES " + \
+                str(year) + "_avg.vrt " + str(year) + "_merged2.tif"
     os.system(merge_cmd)
 
     # ---------- GAP FILLING ---------
     # Fill gaps using
-    gap_fill_cmd = "gdal_fillnodata.py -md 100000 -nomask -si 5 " + str(year) + "_merged.tif " + str(year) + "_filled.tif"
+    gap_fill_cmd = "gdal_fillnodata.py -md 1000 -nomask -si 1 " + str(year) + "_merged.tif " + str(year) + "_filled.tif"
     os.system(gap_fill_cmd)
+
+    print(build_vrt_cmd)
+    print(merge_cmd)
+    print(gap_fill_cmd)
+
+    # Go back to the main working directory
+    #os.chdir(r"../../scripts")
+    return
+
+# --------- GAUSSIAN FILTER ----------
+
+def gaussian_blur(year):
+    # Build a GDAL virtual image from all the rasters
+    build_vrt_cmd = "gdalbuildvrt -srcnodata -999.0 -overwrite -r cubic " + str(year) + "_smoothed.vrt " + \
+                    str(year) + "_filled.tif"
+    os.system(build_vrt_cmd)
+
+    # Add a pixel function to the VRT to average overlapping pixels.
+    # Modified from: https://gis.stackexchange.com/questions/350233/how-can-i-modify-a-vrtrasterband-sub-class-etc-from-python
+    vrt_tree = etree.parse(str(year) + "_smoothed.vrt")
+    vrt_root = vrt_tree.getroot()
+    vrtband1 = vrt_root.findall(".//VRTRasterBand[@band='1']")[0]
+
+    vrtband1.set("subClass", "VRTDerivedRasterBand")
+    pixelFunctionType = etree.Element('PixelFunctionType')
+    pixelFunctionType.text = "gaussian_blur"
+    vrtband1.insert(0, pixelFunctionType)
+    pixelFunctionLanguage = etree.Element('PixelFunctionLanguage')
+    pixelFunctionLanguage.text = "Python"
+    vrtband1.insert(1, pixelFunctionLanguage)
+    pixelFunctionCode = etree.Element('PixelFunctionCode')
+    pixelFunctionCode.text = etree.CDATA("""
+import numpy as np
+from scipy.signal import fftconvolve
+
+def gaussian_blur(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize, buf_radius, gt, **kwargs):
+    print(np.array(in_ar).size)
+    size = 10
+    # expand in_array to fit edge of kernel
+    padded_array = np.pad(in_ar, size, 'symmetric')
+    # build kernel
+    x, y = np.mgrid[-size:size + 1, -size:size + 1]
+    g = np.exp(-(x ** 2 / float(size) + y ** 2 / float(size)))
+    g = (g / g.sum()).astype('Float32')
+    print(g.size)
+    # do the Gaussian blur
+    out_ar = fftconvolve(padded_array, g, mode='valid')
+        """)
+
+    vrtband1.insert(2, pixelFunctionCode)
+    vrt_tree.write(str(year) + "_smoothed.vrt")
+
+    # Merge all the rasters, where pixels overlap - average them
+    merge_cmd = "gdal_translate --config GDAL_VRT_ENABLE_PYTHON YES -a_nodata -999.0 -r cubic " + \
+                str(year) + "_smoothed.vrt " + str(year) + "_smoothed.tif"
+    print('Applying Gaussian Blur filter')
+    os.system(merge_cmd)
 
     # Go back to the main working directory
     os.chdir(r"../../scripts")
@@ -98,14 +204,20 @@ if __name__ == "__main__":
 
     # Get a list of hd5 files in the LVIS file directory
     dir = '/geos/netdata/avtrain/data/3d/oosa/assignment/lvis/' + str(args.year) + '/'
-    files = [f for f in os.listdir(dir) if f.endswith('.h5')]
+    dir2 = '/geos/netdata/avtrain/data/3d/oosa/assignment/lvis/' + str(args.year) + '_additional/'
+    files = [dir + f for f in os.listdir(dir) if f.endswith('.h5')]
+
+    # Append additional files for 2015
+    if args.year == 2015:
+        files = files + [dir2 + f for f in os.listdir(dir2) if f.endswith('.h5')]
+
 
     """
     for file in files:
         gc.collect()
         print('Processing file: ', file)
         # Read in LVIS data within the area of interest
-        lvis = lvisGround(dir + file, minX=256.0, minY=-75.7, maxX=263.0, maxY=-74.0, setElev=True)
+        lvis = lvisGround(file, minX=256.0, minY=-75.7, maxX=263.0, maxY=-74.0, setElev=True)
 
         # If there is data in the ROI, then process it.
         if lvis.data_present:
@@ -127,12 +239,9 @@ if __name__ == "__main__":
             print('memory use:', str(memoryUse)[0:5], 'GB')
     """
 
-    merge_tiles(year=args.year)
 
-    """ TO DO:
-    - also include extra 2015 data
-    - separate gap filling
-    """
+    merge_tiles2(year=args.year)
+    gaussian_blur(year=args.year)
 
     stop = timeit.default_timer()
     print('Processing time: ' + str((stop - start) / 60.0) + ' min')
